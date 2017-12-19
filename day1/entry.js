fbd3fabf947429c59a63acc602c2cab0f73ab760
zhao liang <1723746158@qq.com>
2017-12-19 19:13:26 +0800

    toubu

--- day1/entry.js
+++ day1/entry.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import HeadLz from './components/headLz'
+import Head from './components/head'
 import Cc from './components/cc'
 import "./style/main.css"
 import './style/sty.css'
+import './style/sty1.css'
 class App extends React.Component {
 
 
     render() {
         return ( < div className = "app" >
+            <Head/>
             <
             HeadLz / >
              <Cc/>
